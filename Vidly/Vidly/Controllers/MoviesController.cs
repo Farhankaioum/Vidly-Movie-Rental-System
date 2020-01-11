5db794a79da8523c210a04b21686ea6ab71fa64a
FarhanKaioum <km1472233@gmail.com>
2020-01-12 02:27:52 +0600

    Movie Rental System first commit

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random(int id)
+        {
+            Movie movie = new Movie
+            {
+                Id = 101,
+                Name = "Shrek!"
+            };
+            return View(movie);
+        }
+    }
+}
