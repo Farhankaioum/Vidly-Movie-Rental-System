c05fb554154eb7f98de3b78e24df1ac92cc6b48c
FarhanKaioum <km1472233@gmail.com>
2020-07-24 12:22:14 +0600

    DataAnnotation Added in Customer Name

---
+++ Vidly/Vidly/Migrations/20200724062019_ApplyDataAnnotationToCustomerName.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vidly.Migrations
+{
+    public partial class ApplyDataAnnotationToCustomerName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Customers",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 250);
+        }
+    }
+}
